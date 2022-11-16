13597925d7170f4388b21ac81cfc6d820bde1745
Mohamed Lamine Allal <allalmohamedlamine@gmail.com>
2022-11-16 12:40:38 +0100

    fix: and refactor and update tests and coverage

--- src/index.js
+++ src/index.js
@@ -1,134 +1,145 @@
 const path = require('path');
-const fs = require("fs");
-const json5 = require("json5");
+const fs = require('fs');
+const json5 = require('json5');
 
 const PWD = process.env.PWD || process.cwd();
 
+function last(arr, backStep = 0) {
+  return arr[arr.length - backStep - 1];
+}
+
 function resolveDirPath(pth) {
   if (last(pth) === '/') {
-    return pth.substring(0, pth.length - 1)
+    return pth.substring(0, pth.length - 1);
   }
-  return pth
+  return pth;
 }
 
 function getRootDir() {
-  const configFileArgIndex = process.argv.indexOf('--config-file')
+  const configFileArgIndex = process.argv.indexOf('--config-file');
 
   // config file specified in babel command
   if (configFileArgIndex > 0) {
-    const babelConfigFilePath = process.argv[configFileArgIndex + 1]
+    const babelConfigFilePath = process.argv[configFileArgIndex + 1];
     if (path.isAbsolute(babelConfigFilePath)) {
-      return path.dirname(babelConfigFilePath)
+      return path.dirname(babelConfigFilePath);
     }
     return path.dirname(
-      path.join(
-        path.dirname(module.parent.filename),
-        babelConfigFilePath
-      )
-    )
+      path.join(path.dirname(module.parent.filename), babelConfigFilePath)
+    );
   }
 
-  // no config file in command, so it is required that pwd be the project directory where babelrc is defined
+  /**
+   * no config file in command, calling module (babel.config.js)
+   * or requiring pwd to be where the config file is
+   */
   return path.dirname(module.parent.filename) || PWD;
 }
 
 function readAndParseTsConfig(tsconfigPath) {
-  const tsconfigContent = fs.readFileSync(tsconfigPath, { encoding: 'utf8'})
+  const tsconfigContent = fs.readFileSync(tsconfigPath, { encoding: 'utf8' });
 
-  return json5.parse(tsconfigContent)
-}
-
-function last(arr, backStep = 0) {
-  return arr[arr.length - backStep - 1]
+  return json5.parse(tsconfigContent);
 }
 
 function resolveRoot(config, tsConfig) {
   // If a function then we pass the tsconfig baseUrl. And let the user form what he want
-  if (typeof config?.root === 'function') {
-    return config.root(tsConfig.compilerOptions.baseUrl) || []
+  if (config && typeof config.root === 'function') {
+    const root = config.root(tsConfig.compilerOptions.baseUrl);
+    if (Array.isArray(root)) {
+      return root;
+    }
+    return root ? [root] : [];
   }
 
   // Otherwise we use the baseUrl and merge any extra provided root elements
-  let root = []
-  if (typeof config?.root === 'string') {
-    root = [config.root]
-  } else if(Array.isArray(config?.root)) {
-    root = [...config.root]
+  let root = [];
+  if (config && typeof config.root === 'string') {
+    root = [config.root];
+  } else if (config && Array.isArray(config.root)) {
+    root = [...config.root];
   }
 
-  if (tsConfig.compilerOptions.baseUrl && !root.includes(resolveDirPath(tsConfig.compilerOptions.baseUrl))) {
-    root.push(tsConfig.compilerOptions.baseUrl)
+  if (
+    tsConfig.compilerOptions.baseUrl &&
+    !root.includes(resolveDirPath(tsConfig.compilerOptions.baseUrl))
+  ) {
+    root.push(tsConfig.compilerOptions.baseUrl);
   }
 
-  return root
+  return root;
 }
 
 function resolveAlias(alias, aliasMap) {
   if (last(alias) === '*') {
-    
     return {
-      ['^' + alias.replace('*', '(.*)')]: aliasMap[0].replace('*', '\\1')
-   }
-  } else {
-    /**
-     * There is no *
-     */
-    return {
-      [alias]: aliasMap[0]
-    }
+      [`^${alias.replace('*', '(.*)')}`]: aliasMap[0].replace('*', '\\1')
+    };
   }
+  /**
+   * There is no *
+   */
+  return {
+    [alias]: aliasMap[0]
+  };
 }
 
 /**
- * @param {*} config Optional. Allow us to provide any extra config for the plugin. Including extending root (merge array). And alias prop would be merged too.
+ * @param {*} config Optional. Allow us to provide any extra config for the plugin.
+ * Including extending root (merge array). And alias prop would be merged too.
  * Any other config if it would exist it will be set directly
- * @returns 'babel-plugin-module-resolver' config set to follow typescript tsconfig alias input (baseUrl & paths)
+ * @returns 'babel-plugin-module-resolver' config set to follow typescript tsconfig alias
+ * input (baseUrl & paths)
  */
 function setModuleResolverPluginForTsConfig(config = {}) {
-  let {
-    tsconfigPath,
-    ...restConfig
-  } = config
+  // eslint-disable-next-line prefer-const
+  let { tsconfigPath, ...restConfig } = config;
 
   if (tsconfigPath && !path.isAbsolute(tsconfigPath)) {
-    tsconfigPath = path.join(path.dirname(module.parent.filename), tsconfigPath)
+    tsconfigPath = path.join(
+      path.dirname(module.parent.filename),
+      tsconfigPath
+    );
   }
 
-  tsconfigPath = tsconfigPath || path.join(getRootDir(), 'tsconfig.json')
+  tsconfigPath = tsconfigPath || path.join(getRootDir(), 'tsconfig.json');
 
   if (!fs.existsSync(tsconfigPath)) {
-    throw new Error(tsconfigPath + ' does\'t exist!');
+    throw new Error(`${tsconfigPath} does't exist!`);
   }
 
-  const tsConfig = readAndParseTsConfig(tsconfigPath)
+  const tsConfig = readAndParseTsConfig(tsconfigPath);
 
-  const pluginConfig = {}
+  const pluginConfig = {};
 
   // handle root config
-  const root = resolveRoot(restConfig, tsConfig)
+  const root = resolveRoot(restConfig, tsConfig);
 
   if (root.length) {
-    pluginConfig.root = root
+    pluginConfig.root = root;
   }
 
   // handle alias config
   if (tsConfig.compilerOptions && tsConfig.compilerOptions.paths) {
-    const convertedTsconfigPaths = Object.entries(tsConfig.compilerOptions.paths).reduce((aliases, [alias, aliasMap]) => {
+    const convertedTsconfigPaths = Object.entries(
+      tsConfig.compilerOptions.paths
+    ).reduce((aliases, [alias, aliasMap]) => {
       if (aliasMap.length === 1) {
-        aliases = {
+        return {
           ...aliases,
-          ...resolveAlias(alias, aliasMap),
-        }
-        return aliases
+          ...resolveAlias(alias, aliasMap)
+        };
       }
-  
-      throw new Error('Use one mapping for each alias (tsconfig.json>compilerOptions.paths). Array of length 1.')
-    }, {})
-  
+
+      throw new Error(
+        'Use one mapping for each alias (tsconfig.json>compilerOptions.paths). Array of length 1.'
+      );
+    }, {});
+
     pluginConfig.alias = {
       ...convertedTsconfigPaths,
       ...restConfig.alias
-    }
+    };
   }
 
   return [
@@ -137,10 +148,10 @@ function setModuleResolverPluginForTsConfig(config = {}) {
       ...restConfig,
       ...pluginConfig
     }
-  ]
+  ];
 }
 
 module.exports = {
   setModuleResolverPluginForTsConfig,
   readAndParseTsConfig
-}
+};
--- test/index.test.js
+++ test/index.test.js
@@ -1,140 +1,294 @@
-const { setModuleResolverPluginForTsConfig  } = require('../src');
-const { execSync, exec } = require('child_process'); 
+/* eslint-disable no-undef */
+const { execSync } = require('child_process');
 const path = require('path');
-const fs = require('fs')
+const fs = require('fs');
+const { setModuleResolverPluginForTsConfig } = require('../src');
 
-describe("General test", () => {
+describe('General test', () => {
   function runTest(pluginObj, expectedConfig) {
-    expect(pluginObj.length).toBe(2)
-    expect(typeof pluginObj[0]).toBe('string')
-    expect(pluginObj[1]).toEqual(expectedConfig)
+    expect(pluginObj.length).toBe(2);
+    expect(typeof pluginObj[0]).toBe('string');
+    expect(pluginObj[1]).toEqual(expectedConfig);
   }
+
+  test('Wrong tsconfig file', () => {
+    try {
+      setModuleResolverPluginForTsConfig({
+        tsconfigPath: './wrongPath'
+      });
+      fail('Wrong path an error is expected!');
+    } catch (err) {
+      expect(err.message).toContain("does't exist!");
+    }
+  });
 
   test('test no tsconfig [automatic root detection]', () => {
     /**
-     * The way it is expected is that if not precised in the cli. It would take the caller module directory and take tsconfig.json if there it works. If not it will fail with an error.
+     * The way it is expected is that if not precised in the cli.
+     * It would take the caller module directory and take tsconfig.json if there it works.
+     * If not it will fail with an error.
      */
 
     /**
      * testing when no cli config file arg provided
      */
-    const pluginObj = setModuleResolverPluginForTsConfig()
+    const pluginObj = setModuleResolverPluginForTsConfig();
 
     runTest(pluginObj, {
       root: ['.'],
       alias: {
+        '@fonts': './src/fonts/index.ts',
         '^@utils/(.*)': './src/utils/\\1'
       }
-    })
+    });
 
     /**
      * testing when cli arg provided
      */
 
-    const oldArgv = process.argv
-    process.argv = [...(process.argv || []), '--config-file', './tsconfig.json']
-    const pluginObj2 = setModuleResolverPluginForTsConfig()
-
+    // case: relative path
+    const oldArgv = process.argv;
+    process.argv = [
+      ...(process.argv || []),
+      '--config-file',
+      './tsconfig.json'
+    ];
+    const pluginObj2 = setModuleResolverPluginForTsConfig();
     runTest(pluginObj2, {
       root: ['.'],
       alias: {
+        '@fonts': './src/fonts/index.ts',
         '^@utils/(.*)': './src/utils/\\1'
       }
-    })
+    });
+    process.argv = oldArgv;
 
-    process.argv = oldArgv
-  })
+    // case: absolute path
+    process.argv = [
+      ...(process.argv || []),
+      '--config-file',
+      path.resolve(__dirname, './tsconfig.json')
+    ];
+    const pluginObj3 = setModuleResolverPluginForTsConfig();
+    runTest(pluginObj3, {
+      root: ['.'],
+      alias: {
+        '@fonts': './src/fonts/index.ts',
+        '^@utils/(.*)': './src/utils/\\1'
+      }
+    });
+    process.argv = oldArgv;
+  });
 
   test('test tsconfig and alias', () => {
-    const pluginObj = setModuleResolverPluginForTsConfig({ 
+    const pluginObj = setModuleResolverPluginForTsConfig({
       tsconfigPath: './tsconfig.json',
       alias: {
+        '@fonts': './src/fonts/index.ts',
         '^@hooks/(.*)': './src/hooks/\\1'
       }
-    })
+    });
 
     runTest(pluginObj, {
       root: ['.'],
       alias: {
+        '@fonts': './src/fonts/index.ts',
         '^@utils/(.*)': './src/utils/\\1',
         '^@hooks/(.*)': './src/hooks/\\1'
       }
-    })
-  })
-  
-  test('test tsconfig and same root as tsconfig', () => {
-    const pluginObj = setModuleResolverPluginForTsConfig({ tsconfigPath: './tsconfig.json', root: '.' })
+    });
+  });
+
+  test('test tsconfig and alias', () => {
+    const pluginObj = setModuleResolverPluginForTsConfig({
+      tsconfigPath: './tsconfig.json',
+      alias: {
+        '@fonts': './src/fonts/index.ts',
+        '^@hooks/(.*)': './src/hooks/\\1'
+      }
+    });
 
     runTest(pluginObj, {
       root: ['.'],
       alias: {
-        '^@utils/(.*)': './src/utils/\\1' 
+        '@fonts': './src/fonts/index.ts',
+        '^@utils/(.*)': './src/utils/\\1',
+        '^@hooks/(.*)': './src/hooks/\\1'
       }
-    })
-  })
+    });
+  });
+
+  test('test tsconfig and same root as tsconfig', () => {
+    const pluginObj = setModuleResolverPluginForTsConfig({
+      tsconfigPath: './tsconfig.json',
+      root: '.'
+    });
+
+    runTest(pluginObj, {
+      root: ['.'],
+      alias: {
+        '@fonts': './src/fonts/index.ts',
+        '^@utils/(.*)': './src/utils/\\1'
+      }
+    });
+  });
 
   test('test tsconfig and different root as tsconfig', () => {
-    const pluginObj = setModuleResolverPluginForTsConfig({ tsconfigPath: './tsconfig.json', root: './someDifferent' })
+    const pluginObj = setModuleResolverPluginForTsConfig({
+      tsconfigPath: './tsconfig.json',
+      root: './someDifferent'
+    });
 
     runTest(pluginObj, {
       root: ['./someDifferent', '.'],
       alias: {
-        '^@utils/(.*)': './src/utils/\\1' 
+        '@fonts': './src/fonts/index.ts',
+        '^@utils/(.*)': './src/utils/\\1'
       }
-    })
-  })
+    });
+  });
+
+  test('test tsconfig and and root function', () => {
+    const pluginObj = setModuleResolverPluginForTsConfig({
+      tsconfigPath: './tsconfig.json',
+      root: (/* tsBaseUrl */) => './someDifferent'
+    });
+
+    runTest(pluginObj, {
+      root: ['./someDifferent'],
+      alias: {
+        '@fonts': './src/fonts/index.ts',
+        '^@utils/(.*)': './src/utils/\\1'
+      }
+    });
+
+    const pluginObj2 = setModuleResolverPluginForTsConfig({
+      tsconfigPath: './tsconfig.json',
+      root: (/* tsBaseUrl */) => ['./someDifferent']
+    });
+
+    runTest(pluginObj2, {
+      root: ['./someDifferent'],
+      alias: {
+        '@fonts': './src/fonts/index.ts',
+        '^@utils/(.*)': './src/utils/\\1'
+      }
+    });
+  });
+
+  test('test tsconfig and and root array', () => {
+    const pluginObj = setModuleResolverPluginForTsConfig({
+      tsconfigPath: './tsconfig.json',
+      root: ['./someDifferent', './another']
+    });
+
+    runTest(pluginObj, {
+      root: ['./someDifferent', './another', '.'],
+      alias: {
+        '@fonts': './src/fonts/index.ts',
+        '^@utils/(.*)': './src/utils/\\1'
+      }
+    });
+  });
 
   test('test tsconfig only', () => {
-    const pluginObj = setModuleResolverPluginForTsConfig({ tsconfigPath: './tsconfig.json' })
+    const pluginObj = setModuleResolverPluginForTsConfig({
+      tsconfigPath: './tsconfig.json'
+    });
 
     runTest(pluginObj, {
       root: ['.'],
       alias: {
-        '^@utils/(.*)': './src/utils/\\1' 
+        '@fonts': './src/fonts/index.ts',
+        '^@utils/(.*)': './src/utils/\\1'
       }
-    })
-  })
-})
+    });
+  });
+
+  test('test tsconfig base url with slash at the end', () => {
+    const pluginObj = setModuleResolverPluginForTsConfig({
+      tsconfigPath: './tsconfig.baseUrlWithSlash.json'
+    });
+
+    runTest(pluginObj, {
+      root: ['./'],
+      alias: {
+        '@fonts': './src/fonts/index.ts',
+        '^@utils/(.*)': './src/utils/\\1'
+      }
+    });
+  });
+
+  test('Path alias as an array of multiple values [throw error]', () => {
+    try {
+      setModuleResolverPluginForTsConfig({
+        tsconfigPath: 'tsconfig.invalidAlias.json'
+      });
+      fail(
+        'Expected to fail as there will be multiple values! babel resolver support only one!'
+      );
+    } catch (err) {
+      expect(err.message).toContain(
+        'Use one mapping for each alias (tsconfig.json>compilerOptions.paths). Array of length 1.'
+      );
+    }
+  });
+});
 
 describe('Test fixtures', () => {
   test('that utils alias worked well', () => {
     const fixturesDir = path.join(__dirname, '__fixtures__');
 
-    ['noTsconfig', 'tsconfigAndAlias', 'tsconfigAndRoot', 'tsconfigOnly'].forEach((fixtureDirName) => {
-      const fixtureDir = path.join(fixturesDir, fixtureDirName)
-      const distDir = path.join(fixtureDir, 'dist')
-  
-      execSync(`cd ${fixtureDir} && npx rimraf ${distDir} && npx babel src --out-dir dist --extensions ".ts"`)
-  
-      const indexFilePath = path.join(distDir, 'index.js')
-      expect(fs.existsSync(indexFilePath)).toBeTruthy()
-  
-      const indexFileContent = fs.readFileSync(indexFilePath, { encoding: 'utf8' })
+    [
+      'noTsconfig',
+      'tsconfigAndAlias',
+      'tsconfigAndRoot',
+      'tsconfigOnly'
+    ].forEach((fixtureDirName) => {
+      const fixtureDir = path.join(fixturesDir, fixtureDirName);
+      const distDir = path.join(fixtureDir, 'dist');
+
+      execSync(
+        `cd ${fixtureDir} && npx rimraf ${distDir} && npx babel src --out-dir dist --extensions ".ts"`
+      );
+
+      const indexFilePath = path.join(distDir, 'index.js');
+      expect(fs.existsSync(indexFilePath)).toBeTruthy();
+
+      const indexFileContent = fs.readFileSync(indexFilePath, {
+        encoding: 'utf8'
+      });
       /**
        * NOTE: the webpack configuration and version need to not change.
        * NOTE: Otherwise you may need to change the bellow condition [code style ...]
        */
-      expect(indexFileContent).toContain('require("./utils/logger")')
-    })
-  })
+      expect(indexFileContent).toContain('require("./utils/logger")');
+    });
+  });
   test('that hooks custom added alias worked well', () => {
     const fixturesDir = path.join(__dirname, '__fixtures__');
 
     ['tsconfigAndAlias'].forEach((fixtureDirName) => {
-      const fixtureDir = path.join(fixturesDir, fixtureDirName)
-      const distDir = path.join(fixtureDir, 'dist')
-  
-      execSync(`cd ${fixtureDir} && npx rimraf ${distDir} && npx babel src --out-dir dist --extensions ".ts"`)
-  
-      const indexFilePath = path.join(distDir, 'index.js')
-      expect(fs.existsSync(indexFilePath)).toBeTruthy()
-  
-      const indexFileContent = fs.readFileSync(indexFilePath, { encoding: 'utf8' })
+      const fixtureDir = path.join(fixturesDir, fixtureDirName);
+      const distDir = path.join(fixtureDir, 'dist');
+
+      execSync(
+        `cd ${fixtureDir} && npx rimraf ${distDir} && npx babel src --out-dir dist --extensions ".ts"`
+      );
+
+      const indexFilePath = path.join(distDir, 'index.js');
+      expect(fs.existsSync(indexFilePath)).toBeTruthy();
+
+      const indexFileContent = fs.readFileSync(indexFilePath, {
+        encoding: 'utf8'
+      });
       /**
        * NOTE: the webpack configuration and version need to not change.
        * NOTE: Otherwise you may need to change the bellow condition [code style ...]
        */
-      expect(indexFileContent).toContain('require("./hooks/someSpecialBanana")')
-    })
-  })
-})
+      expect(indexFileContent).toContain(
+        'require("./hooks/someSpecialBanana")'
+      );
+    });
+  });
+});
