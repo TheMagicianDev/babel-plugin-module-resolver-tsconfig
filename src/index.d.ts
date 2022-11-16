dffa991b944e638c133b974f6aeaae5d0c4ffe8d
Mohamed Lamine Allal <allalmohamedlamine@gmail.com>
2022-11-16 14:16:23 +0100

    fix: typing

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,7 +2,7 @@ import type { PluginTarget } from '@babel/core';
 import type { ParsedTsconfig } from 'typescript';
 export declare interface IPluginConfig {
   [configProp: string]: any;
-  root?: string;
+  root?: string | string[] | ((tsConfigBaseUrl: string) => string | string[]);
   alias?: Record<string, string>;
   tsconfigPath?: string;
 }
